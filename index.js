bae0621bcb71b7e93a2745398105386873b297ad
spin~ <spin@seized.email>
2022-08-17 20:27:50 -0700

    Remove unneeded code & fix typo

--- index.js
+++ index.js
@@ -3,27 +3,10 @@
 const { Plugin } = require('powercord/entities');
 
 const ForgImages = {
-  jpg: 'https://cdn.discordapp.com/attachments/788198099067076638/1009655751469441038/forg.jpg',
-  png: 'https://cdn.discordapp.com/attachments/788198099067076638/1009655751721103410/forg.gif',
-  gif: 'https://cdn.discordapp.com/attachments/788198099067076638/1009655752031473755/forg.png'
+  png: 'https://cdn.discordapp.com/attachments/788198099067076638/1009655752031473755/forg.png'
 }
 
 module.exports = class HeyForg extends Plugin {
-  constructor () {
-    super();
-
-    this.URLs = [].concat(
-      Array(3).fill('jpg'),
-      Array(2).fill('gif'),
-      Array(1).fill('png'),
-      Array(4).fill('gif'),
-      Array(3).fill('jpg'),
-      Array(1).fill('png')
-    ).map((format) => (
-      ForgImages[format]
-    ));
-  }
-
   startPlugin () {
     powercord.api.commands.registerCommand({
       command: 'heyforg',
@@ -40,12 +23,12 @@ module.exports = class HeyForg extends Plugin {
   heyforg () {
     document.querySelectorAll('[style*="background-image"]')
       .forEach(({ style }) => (
-        style.backgroundImage = `url("${this.getRandomURL()}")`
+        style.backgroundImage = `url("${ForgImages.png}")`
       ));
 
     document.querySelectorAll('img')
       .forEach(image => (
-        image.src = this.getRandomURL()
+        image.src = ForgImages.png
       ));
   }
 };
